b2aa093a6cc3dae2c841b234f1b0cf338a655973
Pushkar4200 <vermapushkar450@gmail.com>
2025-03-27 18:31:21 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,13 @@
+int fact(int j){
+        if(j==0 || j==1){
+            return j;
+        }
+        else{
+            return j*fact(j%10);
+        }
+    }
+int factorialRange(int star,int end){
+               for(int i=star;i<=end;i++){
+                printf("%d",fact(i));
+               }
+}
