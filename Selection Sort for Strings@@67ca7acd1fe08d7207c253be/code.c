a0162ca6a545d3a8ab2ee4d5e8046c04866a279e
Pushkar4200 <vermapushkar450@gmail.com>
2025-03-29 17:40:02 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-#include<string.h>
-void selectionSort(char arr[][20],int n){
-    char temp[18];
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-        if(strcmp(arr[j],arr[j+1])>0){  
-            strcpy(temp,arr[j]);
-            strcpy(arr[j],arr[j+1]);
-            strcpy(arr[j+1],temp);
-        }
+#include <stdio.h>
+#include <string.h>
+
+void bubbleSort(char arr[][20], int n) {
+    char temp[20];  // Ensure temp can hold strings of up to 20 characters
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j], arr[j + 1]) > 0) {  // Compare strings lexicographically
+                strcpy(temp, arr[j]);             // Swap the strings
+                strcpy(arr[j], arr[j + 1]);
+                strcpy(arr[j + 1], temp);
+            }
         }
     }
 }
-void printArray(char arr[][20],int n){
-    for(int i=0;i<n;i++)
-    printf("%s\n",arr[i]);
-}
+
+void printArray(char arr[][20], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%s\n", arr[i]);
