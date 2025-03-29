4c86c9bf4ded349482350f3047cea43ed1ae4d3f
Pushkar4200 <vermapushkar450@gmail.com>
2025-03-29 18:14:24 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -1,19 +1,26 @@
-#include<stdio.h>
-#include<string.h>
-void insertionSort(char arr[34],int n){
-    char temp[34];
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(strcmp(arr[j],arr[j+1])>0){
-                strcpy(temp,arr[j]);
-                strcpy(arr[j],arr[j+1]);
-                strcpy(arr[j+1],temp);
+#include <stdio.h>
+#include <string.h>
+
+// Function to perform insertion sort on a 2D array of strings
+void insertionSort(char arr[][100], int n) {
+    char temp[100];
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            // Compare adjacent strings using strcmp
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                // Swap strings using strcpy
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[j + 1]);
+                strcpy(arr[j + 1], temp);
             }
         }
     }
 }
-void printArray(char arr[34],int a){
-    for(int i=0;i<a;i++){
-        printf("%s ",arr[i]);
+
+// Function to print the array of strings
+void printArray(char arr[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s ", arr[i]);
     }
+    printf("\n");
 }
